87626e5d9bde16fba97b6a4cec8c7fd7b6826fb1
marube2005 <147842970+marube2005@users.noreply.github.com>
2024-03-15 10:16:23 +0300

    Yes here is just only for Demonstration purposes

---
+++ flipper.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let color = `rgb(${red},${green},${blue})`;
+    document.getElementById("colorFlipper").style.backgroundColor = color;
+}
